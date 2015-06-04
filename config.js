b05d8ce3cef7eb32a95b1c571f224960d1d31f40
jonathan <jonathan.wilfredo.fuentes@gmail.com>
2015-06-03 21:15:15 -0700

    Adding new configuration template

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.mongo = {}
+config.mongo.path = process.env.MONGO_PATH || ""
+
+config.ssl = {}
+config.ssl.pkey = process.env.SSL_PKEY || ""
+config.ssl.cert = process.env.SSL_CERT || ""
